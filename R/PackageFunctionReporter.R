ea36f1dcde8a0738f3ffca57cc6cf21df6875bce
James Lamb <jaylamb20@gmail.com>
2017-11-05 22:13:01 -0600

    Renamed R files for clarity. Moved some internal code around to make dependencies clearer

--- R/PackageFunctionReporter.R
+++ R/PackageFunctionReporter.R
@@ -32,9 +32,10 @@ PackageFunctionReporter <- R6::R6Class(
             self$calculate_network_metrics()
         },
         
+        # TODO [patrick.bouer@uptake.com]: Implement packageTestCoverage metrics
         package_test_coverage = function(packagePath){
             return(invisible(NULL))
-            # TODO [patrick.bouer@uptake.com]: Implement packageTestCoverage metrics
+            
             # log_info('Checking package coverage...')
             # packageObj <- .UpdateNodes(nodes
             #                            , metadataDT = GetCoverageByFunction(pkgPath)) 
@@ -48,3 +49,107 @@ PackageFunctionReporter <- R6::R6Class(
         }
     )
 )
+
+
+# [title] Add metadata to nodes in a package graph
+# [name] .UpdateNodes
+# [description] Given a pkgGraph object created by \code{\link[pkgnet]{ExtractFunctionNetwork}}
+#              and a data.table of metadata, this function will append those metadata to
+#              the internal object used to manage node properties
+# [param] pkgGraph An object created by \code{\link[pkgnet]{ExtractFunctionNetwork}}
+# [param] metadataDT A data.table with node metadata. This table must have a 'node'
+#                   column with the names of nodes (e.g. function names) to be updated.
+# [examples]
+#
+# library(pkgnet)
+# nw <- ExtractFunctionNetwork("ggplot2")
+# 
+# # Add random stuff
+# coverageDT <- data.table(node = c('log.warn', 'GetAPIInfo'), coverage = c(95, 100))
+# newNW <- pkgnet:::.UpdateNodes(nw, coverageDT)
+.UpdateNodes <- function(pkgGraph, metadataDT){
+    
+    # Input checks
+    if (!'nodes' %in% names(pkgGraph)){
+        msg <- paste0("Did you generate pkgGraph with ExtractFunctionNetwork? ",
+                      "It should be a list with a 'nodes' element.")
+        log_fatal(msg)
+    }
+    if (!'data.table' %in% class(pkgGraph[['nodes']])){
+        msg <- "the object in the 'nodes' element of pkgGraph should be a data.table!"
+        log_fatal(msg)
+    }
+    if (!'data.table' %in% class(metadataDT)){
+        msg <- "the object passed to metadataDT should be a data.table!"
+        log_fatal(msg)
+    }
+    if (!'node' %in% names(metadataDT)){
+        msg <- "metadataDT should have a column called 'node'"
+        logal_fatal(msg)
+    }
+    
+    # Append metadata
+    pkgGraph[['nodes']] <- merge(pkgGraph[['nodes']]
+                                 , metadataDT
+                                 , all.x = TRUE
+                                 , all.y = FALSE
+                                 , by = 'node')
+    
+    return(pkgGraph)
+    
+}
+
+
+# [title] Add network measures in a package graph
+# [name] .UpdateNetworkMeasures
+# [description] Given a pkgGraph object created by \code{\link[pkgnet]{ExtractFunctionNetwork}}
+#              and a list of network measures, this function will append the network measures to
+#              the internal network measures object if it is a new measure.  Otherwise, it will 
+#              replace an existing network measure with a new value.
+# [param] pkgGraph An object created by \code{\link[pkgnet]{ExtractFunctionNetwork}}
+# [param] networkMeasureList A list with network measures.
+# [examples]
+#
+# library(pkgnet)
+# nw <- ExtractFunctionNetwork("ggplot2")
+# 
+# # Add random stuff
+# newNetworkMeasure <- list(awesomness = 11)
+# newNW <- pkgnet:::.UpdateNetworkMeasures(nw, newNetworkMeasure)
+#' @importFrom data.table is.data.table
+.UpdateNetworkMeasures <- function(pkgGraph, networkMeasureList){
+    
+    # Input checks
+    if (!'nodes' %in% names(pkgGraph)){
+        msg <- paste0("Did you generate pkgGraph with ExtractFunctionNetwork? ",
+                      "It should be a list with a 'nodes' element.")
+        log_fatal(msg)
+    }
+    if (!data.table::is.data.table(pkgGraph[['nodes']])){
+        msg <- "the object in the 'nodes' element of pkgGraph should be a data.table!"
+        log_fatal(msg)
+    }
+    if (!is(networkMeasureList, "list")){
+        msg <- "the object passed to networkMeasureList should be a list!"
+        log_fatal(msg)
+    }
+    
+    # replace Value is exists already. otherwise append
+    currentNames <- names(pkgGraph[['networkMeasures']])
+    newNames <- names(networkMeasureList)
+    existingIX <- match(x = newNames, table = currentNames)
+    r <- 0
+    for(i in existingIX){
+        r <- r + 1
+        if(is.na(i)) {
+            #append
+            pkgGraph[['networkMeasures']] <- c(pkgGraph[['networkMeasures']], networkMeasureList[r])
+        } else {
+            #replace
+            pkgGraph[['networkMeasures']][i] <- networkMeasureList[r]
+        }
+    }
+    
+    return(pkgGraph)
+    
+}
