16395f51800cb2d4973a10dd70f16890b3cfba69
Brian Burns <brian.burns@uptake.com>
2018-01-26 00:09:10 -0600

    update unit tests

--- tests/testthat/test-CreatePackageReport.R
+++ tests/testthat/test-CreatePackageReport.R
@@ -1,6 +1,12 @@
 context("CreatePackageReport")
 
-#Supress logger in tests
+# Configure logger (suppress all logs in testing)
+loggerOptions <- futile.logger::logger.options()
+if (!identical(loggerOptions, list())){
+  origLogThreshold <- loggerOptions[[1]][['threshold']]
+} else {
+  origLogThreshold <- futile.logger::INFO
+}
 futile.logger::flog.threshold(0,name=futile.logger::flog.namespace())
 
 test_that("Test that CreatingPackageReport Runs", {
@@ -12,3 +18,10 @@ test_that("Test that CreatingPackageReport Runs", {
     testthat::expect_true(file.exists("test_plots.pdf") && file.size("test_plots.pdf") > 0)
     file.remove("test_plots.pdf")
 })
+
+
+##### TEST TEAR DOWN #####
+
+futile.logger::flog.threshold(origLogThreshold)
+rm(list = ls())
+closeAllConnections()
--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,18 +1,30 @@
-context("Plotting")
+context("Plotting Tests")
 
+##### TEST SET UP #####
+
+rm(list = ls())
+# Configure logger (suppress all logs in testing)
+# expect_silents only work with this logger turned off; only alerts with warnings
+loggerOptions <- futile.logger::logger.options()
+if (!identical(loggerOptions, list())){
+  origLogThreshold <- loggerOptions[[1]][['threshold']]
+} else {
+  origLogThreshold <- futile.logger::INFO
+}
+futile.logger::flog.threshold(0)
 
 test_that('node coloring by discrete and continuous', {
   b <- PackageFunctionReporter$new()
   b$set_package('baseballstats', packagePath = system.file('baseballstats',package="pkgnet"))
-  b$calculate_metrics()
+  b$calculate_all_metrics()
   b$get_raw_data()
   b$set_plot_node_color_scheme(field = "coverageRatio"
                                , pallete = c("red", "green")
   )
   
   expect_silent(object = b$plot_network())
-                #, regexp = "Done creating plot"
-               #, info = "Plot with continuous coloring has issues")
+               #  , regexp = "Coloring plot nodes by coverageRatio"
+               # , info = "Plot with continuous coloring has issues")
   
   b$set_plot_node_color_scheme(field = "filename"
                                , pallete = c("#E41A1C"
@@ -27,7 +39,13 @@ test_that('node coloring by discrete and continuous', {
   )
   
   expect_silent(object = b$plot_network())
-                # , regexp = "Done creating plot"
-                # , info = "Plot with continuous coloring has issues")
+                 # , regexp = "Coloring plot nodes by filename"
+                 # , info = "Plot by character fields has issues")
   
 })
+
+##### TEST TEAR DOWN #####
+
+futile.logger::flog.threshold(origLogThreshold)
+rm(list = ls())
+closeAllConnections()
