d2dbe558f6a9b10a89e854cd4ff91029bbe5b62f
James Lamb <jaylamb20@gmail.com>
2018-09-15 13:39:10 -0500

    fixed empty test

--- tests/testthat/test-CreatePackageReport.R
+++ tests/testthat/test-CreatePackageReport.R
@@ -10,17 +10,17 @@ if (!identical(loggerOptions, list())){
 futile.logger::flog.threshold(0,name=futile.logger::flog.namespace())
 
 test_that("Test that CreatePackageReport Runs", {
-    
+
     testReportPath <- tempfile(
         pattern = "baseball"
         , fileext = ".html"
     )
-    
+
     reporters <- CreatePackageReport(
         pkg_name = "baseballstats"
         , report_path = testReportPath
     )
-    
+
     testthat::expect_true(all(unlist(lapply(reporters, function(x) "AbstractPackageReporter" %in% class(x)))))
     testthat::expect_true(file.exists(testReportPath) && file.size(testReportPath) > 0)
     testthat::expect_named(object = reporters
@@ -30,14 +30,14 @@ test_that("Test that CreatePackageReport Runs", {
 })
 
 test_that("CreatePackageReport rejects bad inputs to reporters", {
-    
+
     expect_error({
         CreatePackageReport(
             pkg_name = "baseballstats"
             , pkg_reporters = list(a = rnorm(100))
         )
     }, regexp = "At least one of the reporters in the pkg_reporters parameter is not a PackageReporter")
-    
+
 })
 
 test_that("CreatePackageReport rejects bad packages with an informative error", {
@@ -48,35 +48,38 @@ test_that("CreatePackageReport rejects bad packages with an informative error", 
     }, regexp = "pkgnet could not find a package called 'w0uldNEverB33aPackageName'")
 })
 
-test_that("CeratePackageReport rejects bad pkg_path arguments", {
-    CreatePackageReport(
-        pkg_name = "baseballstats"
-    )
+test_that("CreatePackageReport rejects bad pkg_path arguments", {
+    expect_error({
+        CreatePackageReport(
+            pkg_name = "baseballstats"
+            , pkg_path = "over_there"
+        )
+    })
 })
 
 
-test_that("CeratePackageReport rejects bad report_path arguments", {
-    
-    
+test_that("CreatePackageReport rejects bad report_path arguments", {
+
+
     expect_error({
         CreatePackageReport(
             pkg_name = "base"
             , report_path = "~"
-        )  
+        )
     }, regexp = "report_path must be a \\.html file")
-    
+
     expect_error({
         CreatePackageReport(
             pkg_name = "base"
             , report_path = "blegh.pdf"
         )
     }, regexp = "report_path must be a \\.html file")
-    
+
     expect_error({
         CreatePackageReport(
             pkg_name = "base"
             , report_path = list("blegh.html")
-        )  
+        )
     }, regexp = "report_path is not a string")
 })
 
