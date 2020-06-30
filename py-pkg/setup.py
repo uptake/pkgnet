b96f408a4b1c5b5208483b5218d3acd2fe1a7d1c
Jay Qi <jayqi@users.noreply.github.com>
2020-06-29 20:50:15 -0700

    Requirements

--- py-pkg/setup.py
+++ py-pkg/setup.py
@@ -1,4 +1,21 @@
+from pathlib import Path
 from setuptools import setup
+
+INSTALL_REQUIREMENTS_FILE = Path(__file__).parent / "requirements.txt"
+
+
+def load_requirements(path: Path):
+    requirements = []
+    with path.open("r") as fp:
+        for line in fp.readlines():
+            if line.startswith("-r"):
+                requirements += load_requirements(line.split(" ")[1].strip())
+            else:
+                requirement = line.strip()
+                if requirement and not requirement.startswith("#"):
+                    requirements.append(requirement)
+    return requirements
+
 
 setup(
     name="pkgnet",
@@ -7,8 +24,8 @@ setup(
     url="",
     author="",
     author_email="",
+    install_requires=load_requirements(INSTALL_REQUIREMENTS_FILE),
     license="BSD",
     packages=["pkgnet"],
     zip_safe=False,
-    install_requires=["jinja2", "networkx", "pandas", "setuptools"],
 )
