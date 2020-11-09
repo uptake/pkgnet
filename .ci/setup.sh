85adddd0676f321e885fa806953f4538edd6fbf7
James Lamb <jaylamb20@gmail.com>
2020-11-09 00:11:43 -0600

    use test.sh

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-export _R_CHECK_CRAN_INCOMING_=false
-
 packages="
   c('assertthat', 'covr', 'data.table', 'DT', 'futile.logger', 'ggplot2',
     'glue', 'igraph', 'knitr', 'magrittr', 'methods', 'pkgdown', 'R6', 'roxygen2',
@@ -40,25 +38,4 @@ elif [[ "${OS_NAME}" == "linux" ]]; then
             qpdf \
         || exit -1
     Rscript -e "install.packages(${packages}, repos = 'https://cran.r-project.org')"
-fi
-
-R CMD build .
-R CMD check --as-cran *.tar.gz
-
-LOG_FILE_NAME="pkgnet.Rcheck/00check.log"
-
-if grep -q -R "WARNING" "$LOG_FILE_NAME"; then
-    echo "WARNINGS have been found by R CMD check!"
-    exit 1
-fi
-
-ALLOWED_CHECK_NOTES=0
-NUM_CHECK_NOTES=$(
-    cat ${LOG_FILE_NAME} \
-        | grep -e '^Status: .* NOTE.*' \
-        | sed 's/[^0-9]*//g'
-)
-if [[ ${NUM_CHECK_NOTES} -gt ${ALLOWED_CHECK_NOTES} ]]; then
-    echo "Found ${NUM_CHECK_NOTES} NOTEs from R CMD check. Only ${ALLOWED_CHECK_NOTES} are allowed"
-    exit 1
 fi
