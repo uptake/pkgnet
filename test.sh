a2f3b78e8dc40257a2c193a1621d82f8ebe7eb83
James Lamb <jaylamb20@gmail.com>
2020-11-09 21:38:28 -0600

    poppin

--- test.sh
+++ test.sh
@@ -13,22 +13,22 @@ cp *.tar.gz ~/pkgnet_test_dir
 export _R_CHECK_CRAN_INCOMING_=false
 pushd ~/pkgnet_test_dir
     R CMD CHECK *.tar.gz --as-cran
+
+    LOG_FILE_NAME="pkgnet.Rcheck/00check.log"
+
+    if grep -q -R "WARNING" "$LOG_FILE_NAME"; then
+        echo "WARNINGS have been found by R CMD check!"
+        exit 1
+    fi
+
+    ALLOWED_CHECK_NOTES=0
+    NUM_CHECK_NOTES=$(
+        cat ${LOG_FILE_NAME} \
+            | grep -e '^Status: .* NOTE.*' \
+            | sed 's/[^0-9]*//g'
+    )
+    if [[ ${NUM_CHECK_NOTES} -gt ${ALLOWED_CHECK_NOTES} ]]; then
+        echo "Found ${NUM_CHECK_NOTES} NOTEs from R CMD check. Only ${ALLOWED_CHECK_NOTES} are allowed"
+        exit 1
+    fi
 popd
-
-LOG_FILE_NAME="pkgnet.Rcheck/00check.log"
-
-if grep -q -R "WARNING" "$LOG_FILE_NAME"; then
-    echo "WARNINGS have been found by R CMD check!"
-    exit 1
-fi
-
-ALLOWED_CHECK_NOTES=0
-NUM_CHECK_NOTES=$(
-    cat ${LOG_FILE_NAME} \
-        | grep -e '^Status: .* NOTE.*' \
-        | sed 's/[^0-9]*//g'
-)
-if [[ ${NUM_CHECK_NOTES} -gt ${ALLOWED_CHECK_NOTES} ]]; then
-    echo "Found ${NUM_CHECK_NOTES} NOTEs from R CMD check. Only ${ALLOWED_CHECK_NOTES} are allowed"
-    exit 1
-fi
