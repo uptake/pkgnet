03c4480be6562db92553485716adc978c85a12c8
James Lamb <jaylamb20@gmail.com>
2020-11-09 21:16:45 -0600

    misc changes

---
+++ .github/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+if [[ "${OS_NAME}" == "macos" ]]; then
+    brew install \
+        checkbashisms \
+        qpdf
+    # brew cask install basictex
+    # export PATH="/Library/TeX/texbin:$PATH"
+    # sudo tlmgr --verify-repo=none update --self
+    # sudo tlmgr --verify-repo=none install inconsolata helvetic
+elif [[ "${OS_NAME}" == "linux" ]]; then
+    sudo apt-get update
+    sudo apt-get install \
+        --no-install-recommends \
+        -y \
+        --allow-downgrades \
+            libcurl4-openssl-dev \
+            libfribidi-dev \
+            libharfbuzz-dev \
+            curl \
+            devscripts \
+            # texinfo \
+            # texlive-latex-recommended \
+            # texlive-fonts-recommended \
+            # texlive-fonts-extra \
+            # qpdf \
+        || exit -1
+fi
+
+Rscript -e "
+    options(install.packages.check.source = 'no');
+    install.packages('remotes', repos = 'https://cran.r-project.org')
+    remotes::install_deps(dependencies = TRUE)
+"
