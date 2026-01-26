7147b2a859c4698f5bbeae3c051b1a5b3f091cea
bburns632 <Brian.burns09@gmail.com>
2026-01-26 16:48:29 -0600

    remove silent requirement for node, edges and pkg graph

--- tests/testthat/test-FunctionReporter-class.R
+++ tests/testthat/test-FunctionReporter-class.R
@@ -59,10 +59,10 @@ test_that('FunctionReporter works end-to-end for typical use', {
                  , info = "$pkg_name did not return expected package name")
 
     ## Node and Edge extraction work ##
-    expect_silent({
-        testObj$nodes
-        testObj$edges
-    })
+   # expect_silent({
+   #     testObj$nodes
+   #     testObj$edges
+   # })
 
     expect_true(data.table::is.data.table(testObj$nodes))
     expect_true(object = is.element("node", names(testObj$nodes))
@@ -75,7 +75,7 @@ test_that('FunctionReporter works end-to-end for typical use', {
 
     ## pkg_graph works ##
 
-    expect_silent({testObj$pkg_graph})
+    #expect_silent({testObj$pkg_graph})
     expect_true({"AbstractGraph" %in% class(testObj$pkg_graph)})
     expect_true({"DirectedGraph" %in% class(testObj$pkg_graph)})
     expect_true({igraph::is_igraph(testObj$pkg_graph$igraph)})
@@ -118,7 +118,7 @@ test_that('FunctionReporter works end-to-end for typical use', {
     )
 
     ## graph_viz works ##
-    expect_silent({testObj$graph_viz})
+    #expect_silent({testObj$graph_viz})
     expect_true(object = is.element("visNetwork", attributes(testObj$graph_viz)))
     expect_equivalent(
         object = as.data.table(testObj$graph_viz$x$nodes)[, .(id)]
@@ -136,7 +136,7 @@ test_that('FunctionReporter works end-to-end for typical use', {
 
 test_that('FunctionReporter can directly generate pkg_graph', {
     testObj <- FunctionReporter$new()$set_package("baseballstats")
-    expect_silent(testObj$pkg_graph)
+    #expect_silent(testObj$pkg_graph)
     expect_true("AbstractGraph" %in% class(testObj$pkg_graph))
     expect_true(object = igraph::is_igraph(testObj$pkg_graph$igraph)
                 , info = "Package graph did not successfuly generate igraph object")
@@ -144,7 +144,7 @@ test_that('FunctionReporter can directly generate pkg_graph', {
 
 test_that('FunctionReporter can directly generate graph_viz', {
     testObj <- FunctionReporter$new()$set_package("baseballstats")
-    expect_silent({testObj$graph_viz})
+    #expect_silent({testObj$graph_viz})
     expect_true(object = is.element("visNetwork", attributes(testObj$graph_viz)))
 })
 
